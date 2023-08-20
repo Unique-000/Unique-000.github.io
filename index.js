de4063b2bf6aca1d0d55e7b7755bfdee0ac39105
Unique-000 <126262861+Unique-000@users.noreply.github.com>
2023-08-20 23:43:23 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function ScrollToProjects(){
+    window.scrollTo({ top: 700, left: 0, behavior: 'smooth' });
+}
+function ScrollToExperience(){
+    window.scrollTo({ top: 1200, left: 0, behavior: 'smooth' });
+}
+function ScrollToContact(){
+    window.scrollTo({ top: 1500, left: 0, behavior: 'smooth' });
+}
